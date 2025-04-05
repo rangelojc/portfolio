ff0299874ea99d5a79937618e44c06139c0712b9
Ramce Concepcion <ramuzuconcepcion@gmail.com>
2025-04-05 19:47:43 +0800

    feat: restructure application with React Router and Tailwind CSS integration

---
+++ src/pages/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-xl text-red-500">Foo</div>;
+}
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/portfolio/",
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
+  },
 });
